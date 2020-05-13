c01321ec3e10a31bd313bfec169e45a669b352cf
Brad Fujimoto <bradjordan@gmail.com>
2020-05-13 16:45:07 +0200

    exo 3 wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+Flat.create!([
+  { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3 },
+  { name: 'Dark and dreary', address: '30 Kensington London W9 1DT', description: 'Big open space', price_per_night: 100, number_of_guests: 2 },
+  { name: 'Happy and cheery', address: '20 Soho London W9 1DT', description: 'Lots of natural light', price_per_night: 150, number_of_guests: 2 },
+  { name: 'The best flat ever', address: '15 Charing Cross W9 1DT', description: 'Unbelievable you have to see it', price_per_night: 300, number_of_guests: 4 }
+])
